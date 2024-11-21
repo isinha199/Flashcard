4d9968871752dbef1fb65c9f9b4e6266a072feb1
isinha199 <00001117673133sp@al.educacao.sp.gov.br>
2024-11-21 19:58:22 -0300

    Flashcard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha?',
+    'A capital da Espanha é Madrid'
+)
+
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação que permite criar conteúdo dinâmico, controlar mídias, e animar imagens.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz estudo em Inglês?',
+    'Estudo em ingles é study'
+)
